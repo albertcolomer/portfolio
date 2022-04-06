2fe18d55a87c6672cba6112888cebe45cb614119
albertcolomer <colomermr@hotmail.com>
2022-04-06 03:07:35 +0200

    language correction

--- src/app/certificates/certificates.component.ts
+++ src/app/certificates/certificates.component.ts
@@ -17,13 +17,13 @@ export class CertificatesComponent implements OnInit {
   dataCertificates=[
     {
       'insignia':'assets/image/azure.png',
-      'name':'AZ-900 AZUIRE FUNDAMENTALS',
+      'name':'AZ-900 AZURE FUNDAMENTALS',
       'code':'',
       'provider':'assets/image/credly.jpg',
       'details':'https://www.credly.com/badges/65aaadbc-4126-461a-9bb7-84e8a9b8e08c/public_url',
     },{
       'insignia':'  ',
-      'name':'IFCT0609 PROGRAMACIÓN DE SISTEMAS INFORMÁTICOS',
+      'name':'IFCT0609 PROGRAMING INFORMATION SYSTEMS',
       'code':'code: 0982523646798833348167',
       'provider':'assets/image/madrid-28525_640.png',
       'details':'http://www.madrid.org/csv',
