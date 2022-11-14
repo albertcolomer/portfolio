91946a3550959ff8967a21b53bfac349deecc0be
albertcolomer <colomermr@hotmail.com>
2022-11-14 01:27:54 +0100

    project mrtk

--- src/app/projects/projects.constant.ts
+++ src/app/projects/projects.constant.ts
@@ -1,5 +1,17 @@
 export const projects = [
   {
+    title: 'Rover Explore',
+    skills:'https://img.icons8.com/dusk/30/000000/unity.png', 
+    skill2:"https://img.icons8.com/color/30/null/webpack.png",
+    skill3: "https://img.icons8.com/color/30/null/visual-studio--v2.png",
+    javascript:'https://img.icons8.com/color/30/000000/c-sharp-logo.png',
+    typescript:"https://img.icons8.com/color/30/000000/blender-3d.png",
+    
+    image: 'assets/image/RoverExplore.png',
+    demo: 'https://albertcolomer.github.io/webxrmrtkRover/',
+    github: 'https://github.com/albertcolomer/webxrmrtkRover',
+  },
+  {
     title: 'Portfolio',
     skills:'https://img.icons8.com/color/30/000000/html-5--v1.png', 
     skill2:"https://img.icons8.com/color/30/000000/angularjs.png",
