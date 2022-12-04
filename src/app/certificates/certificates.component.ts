d454b4001c0d1fc81e2386310e0a2aadedbe9b68
albertcolomer <colomermr@hotmail.com>
2022-12-04 15:09:59 +0100

    edit certificates

--- src/app/certificates/certificates.component.ts
+++ src/app/certificates/certificates.component.ts
@@ -17,25 +17,25 @@ export class CertificatesComponent implements OnInit {
   dataCertificates=[
     {
       'insignia':'assets/image/azure.png',
-      'name':'AZ-900 AZURE FUNDAMENTALS',
+      'name':'AZ-900, AZURE FUNDAMENTALS',
       'code':'',
       'provider':'assets/image/credly.jpg',
       'details':'https://www.credly.com/badges/65aaadbc-4126-461a-9bb7-84e8a9b8e08c/public_url',
     },{
-      'insignia':'  ',
-      'name':'IFCT0609 COMPUTER SYSTEMS PROGRAMING',
-      'code':'code: 0982523646798833348167',
+      'insignia':'assets/image/Loeches.gif',
+      'name':'IFCT0609, COMPUTER SYSTEMS PROGRAMING',
+      'code':'0982523646798833348167',
       'provider':'assets/image/madrid-28525_640.png',
       'details':'http://www.madrid.org/csv',
     },{
       'insignia':'assets/image/logo-CESUR-300x261.png',
-      'name':'IFCT078PO PATRONES SOFTWARE',
+      'name':'IFCT078PO, PATRONES SOFTWARE',
       'code':' ',
       'provider':'https://img.icons8.com/color/64/pdf-2.png',
       'details':'assets/image/IFCT078PO___PATRONES_SOFTWAR.pdf',
     },{
-      'insignia':'assets/image/LogoCeinpro.jfif',
-      'name':'IFCD91 AR, VR, MR in environments 4.0',
+      'insignia':'assets/image/ceinpro.png',
+      'name':'IFCD91, AR/ VR/ MR in environments 4.0',
       'code':' ',
       'provider':'https://img.icons8.com/color/64/pdf-2.png',
       'details':'assets/image/Diploma VRAR.pdf',
